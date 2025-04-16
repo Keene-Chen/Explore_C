f0324f614d40dd9fbd100e9c22eb88fcae8adb81
KeeneChen <keenechen@qq.com>
2025-04-16 23:33:13 +0800

    feat(Test): 实现一个数学运算结构体
    - 新增一个名为 test_t 的结构体，包含基本的数学运算函数指针
    - 实现了加法、减法、乘法和除法函数
    - 在 main 函数中使用该结构体进行数学运算并输出结果
    - 删除了原有的变量声明和指针操作代码

--- Test/1.c
+++ Test/1.c
@@ -1,14 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct {
+    char name[10];
+    int age;
+    double (*add)(double x, double y);
+    double (*sub)(double x, double y);
+    double (*mul)(double x, double y);
+    double (*division)(double x, double y);
+} test_t;
+
+static double add(double x, double y)
+{
+    return x + y;
+}
+
+static double sub(double x, double y)
+{
+    return x - y;
+}
+
+static double mul(double x, double y)
+{
+    return x * y;
+}
+
+static double division(double x, double y)
+{
+    return x / y;
+}
+
 int main(void)
 {
-    int a = 10;
-    int* p = (int*)malloc(sizeof(int));
-    *p = 20;
+    test_t test = {
+        .add      = add,
+        .sub      = sub,
+        .mul      = mul,
+        .division = division,
+    };
 
-    printf("%d %d\n", a, *p);
-    free(p);
+    printf("%f\n", test.add(1, 2));
+    printf("%f\n", test.sub(1, 2));
+    printf("%f\n", test.mul(1, 2));
+    printf("%f\n", test.division(1, 2));
 
     return 0;
 }
