7bc7020cfa8581199b986e11e89f78913c20fd6c
KeeneChen <keenechen@qq.com>
2025-04-16 23:33:54 +0800

    build(Grammar/01_beautiful_world): 添加 CMakeLists 文件并重命名 hello.c
    - 在 01_beautiful_world 目录下新增 CMakeLists.txt 文件
    - 重命名 hello.c 文件，保持文件名不变但更新了路径
    - 在 Grammar 目录下新增 CMakeLists.txt，添加子目录 01_beautiful_world

---
+++ Grammar/01_beautiful_world/hello.c
@@ -0,0 +1,15 @@
+/**
+ * @file    : hello.c
+ * @author  : KeeneChen <keenechen@qq.com>
+ * @date    : 2022.07.01-14:55:50
+ * @details : hello
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello World\n");
+
+    return 0;
+}
